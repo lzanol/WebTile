14cea5185b7dc8665fba1cde6fc243a32a7de6c2
Leandro Zanol <lzanol@gmail.com>
2014-06-19 21:01:23 -0300

    Refacts and fixes

--- src/js/webtile.js
+++ src/js/webtile.js
@@ -75,99 +75,6 @@ Time.prototype.getCurrent = function() {
 	return this.getCurrentMs()*.001
 }
 
-function VisualObj() {
-	VisualObj.instances.push(this)
-
-	this.shape = document.createElement("div")
-	this.shape.style.position = "absolute"
-	this.pos = new Point()
-	this.posLast = new Point()
-	this.size = new Point()
-	this.sizeHalf = new Point()
-	this.indexes = new Point()
-	this.time = new Time()
-
-	var i,vot,inst
-
-	if (isNaN(VisualObj.updateId))
-		VisualObj.updateId = setInterval(function() {
-			vot = VisualObj.instances.length
-
-			for (i = 0; i < vot; ++i)
-				Utils.createDelegate(inst = VisualObj.instances[i], inst.onUpdate)()
-		}, 1000/VisualObj.FPS)
-}
-
-VisualObj.TILE_SIZE = 30
-VisualObj.FPS = 50
-
-VisualObj.updateId = NaN
-VisualObj.instances = []
-
-VisualObj.prototype.shape = null
-VisualObj.prototype.pos = null
-VisualObj.prototype.posLast = null
-VisualObj.prototype.size = null
-VisualObj.prototype.sizeHalf = null
-VisualObj.prototype.indexes = null
-VisualObj.prototype.time = null
-VisualObj.prototype.onUpdate = function() {}
-
-VisualObj.prototype.setPos = function(pos) {
-	this.shape.style.left = (pos.x - this.sizeHalf.x) + "px"
-	this.shape.style.top = (pos.y - this.sizeHalf.y) + "px"
-
-	this.posLast.x = this.pos.x
-	this.posLast.y = this.pos.y
-	this.pos.x = pos.x
-	this.pos.y = pos.y
-}
-
-VisualObj.prototype.getPos = function() {
-	return new Point(this.pos)
-}
-
-VisualObj.prototype.setSize = function(size) {
-	this.shape.style.width = size.x + "px"
-	this.shape.style.height = size.y + "px"
-	this.size.x = size.x
-	this.size.y = size.y
-	this.sizeHalf.x = size.x/2
-	this.sizeHalf.y = size.y/2
-}
-
-VisualObj.prototype.getSize = function() {
-	return new Point(this.size)
-}
-
-VisualObj.prototype.getIndexes = function(p) {
-	if (p == null)
-		p = this.pos
-
-	if (p.x != this.posLast.x)
-		this.indexes.x = Math.floor(p.x/VisualObj.TILE_SIZE)
-
-	if (p.y != this.posLast.y)
-		this.indexes.y = Math.floor(p.y/VisualObj.TILE_SIZE)
-
-	return this.indexes
-}
-
-VisualObj.prototype.isColliding = function(p) {
-	return p.x >= this.pos.x - this.sizeHalf.x &&
-		p.x <= this.pos.x + this.sizeHalf.x &&
-		p.y >= this.pos.y - this.sizeHalf.y &&
-		p.y <= this.pos.y + this.sizeHalf.y
-}
-
-VisualObj.prototype.destroy = function() {
-	delete this.onUpdate
-
-	this.shape.parentNode.removeChild(this.shape)
-
-	delete this
-}
-
 function WebTile(container, map, sprites, loadCallback) {
 	this.container = container
 	this.map = map
@@ -179,7 +86,8 @@ function WebTile(container, map, sprites, loadCallback) {
 
 WebTile.TILE_SIZE = 16
 WebTile.MASK_FLOOR = 0xffffff ^ WebTile.TILE_SIZE - 1
-WebTile.BITS_DIV = Math.log(WebTile.TILE_SIZE)/Math.log(2)
+WebTile.MASK_CROP = (WebTile.TILE_SIZE << 1) - 1
+WebTile.BIT_EXP = Math.log(WebTile.TILE_SIZE)/Math.log(2)
 
 WebTile.prototype.container = null
 WebTile.prototype.map = null
@@ -193,7 +101,7 @@ WebTile.prototype.init = function() {
 
 	this.onResize()
 
-	//this.container.style.overflow = "hidden"
+	this.container.style.overflow = "hidden"
 	this.grid = document.createElement("div")
 	this.grid.style.position = "relative"
 	this.tiles = []
@@ -207,8 +115,8 @@ WebTile.prototype.init = function() {
 			s.position = "absolute"
 			s.width = s.height = WebTile.TILE_SIZE + "px"
 			s.backgroundRepeat = "no-repeat"
-			s.left = c*16 + "px"
-			s.top = r*16 + "px"
+			s.left = (c << WebTile.BIT_EXP) + "px"
+			s.top = (r << WebTile.BIT_EXP) + "px"
 
 			this.tiles[r].push(t)
 			this.grid.appendChild(t)
@@ -224,8 +132,8 @@ WebTile.prototype.init = function() {
 }
 
 WebTile.prototype.onResize = function() {
-	this.ct = ((this.container.offsetWidth & WebTile.MASK_FLOOR) >> WebTile.BITS_DIV) + 1
-	this.rt = ((this.container.offsetHeight & WebTile.MASK_FLOOR) >> WebTile.BITS_DIV) + 1
+	this.ct = (this.bwCeil(this.container.offsetWidth) >> WebTile.BIT_EXP) + 1
+	this.rt = (this.bwCeil(this.container.offsetHeight) >> WebTile.BIT_EXP) + 1
 }
 
 WebTile.prototype.update = function() {
@@ -235,4 +143,9 @@ WebTile.prototype.update = function() {
 		for (c = 0; c < this.ct; ++c)
 			if (r < this.map.length && c < this.map[r].length && this.map[r][c] < this.sprites.length)
 				this.tiles[r][c].style.backgroundImage = "url(" + this.sprites[this.map[r][c]] + ")"
+}
+
+WebTile.prototype.bwCeil = function(n) {
+	return n & WebTile.MASK_CROP == WebTile.TILE_SIZE ?
+		n : n + WebTile.TILE_SIZE & WebTile.MASK_FLOOR
 }
